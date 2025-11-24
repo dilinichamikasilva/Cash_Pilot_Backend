6861ac4ab80b4ac9b6c5fb678ec63d5f5527ed90
dilini <dilinichamikasilva@gmail.com>
2025-11-24 19:42:00 +0530

    feat(auth):added cors and mongoose to the index.ts and created auth routes
    install cors
    created auth routes

---
+++ src/index.ts
@@ -0,0 +1,35 @@
+import express from "express"
+import cors from "cors"
+import dotenv from "dotenv"
+import mongoose, { mongo } from "mongoose"
+import authRoutes from "./routes/auth.routes"
+dotenv.config()
+
+const SERVER_PORT = process.env.SERVER_PORT
+const MONGO_URI = process.env.MONGO_URI as string
+
+const app = express()
+
+app.use(express.json())
+app.use(
+    cors({
+        origin: ["http://localhost:5173"],
+        methods: ["GET", "POST", "PUT", "DELETE"]
+    })
+)
+
+app.use("/api/v1/auth" , authRoutes)
+
+mongoose
+    .connect(MONGO_URI)
+    .then(() => {
+        console.log("DB has connected!")
+    })
+    .catch((err) => {
+        console.log(`DB connection has failed: ${err}`)
+        process.exit(1)
+    })
+
+app.listen(SERVER_PORT, () => {
+    console.log(`Server is running on ${SERVER_PORT}`)
+})
