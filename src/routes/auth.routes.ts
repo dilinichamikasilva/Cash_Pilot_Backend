e7f6b3d7847b78759001ecb307ef0fc16ff03b28
dilini <dilinichamikasilva@gmail.com>
2025-11-24 20:04:33 +0530

    feat(auth):add some code in auth.routes

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,6 +1,12 @@
 import {Router} from "express"
-
+import {registerUser , loginUser , refreshToken , getMe} from "../controller/auth.controller"
+import { authMiddleWare } from "../middleware/auth.middleware"
 
 const router = Router()
+
+router.post("/register" , registerUser)
+router.post("/login" , loginUser)
+router.post("/refresh-token" , refreshToken)
+router.post("/me" , authMiddleWare ,  getMe)
 
 export default router
