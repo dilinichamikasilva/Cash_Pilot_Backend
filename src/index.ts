002fa0ec57406d66b78a1b0385014ae25f4275c1
dilini <dilinichamikasilva@gmail.com>
2026-01-04 23:47:17 +0530

    fix: update CORS origin to include localhost for development

--- src/index.ts
+++ src/index.ts
@@ -23,7 +23,7 @@ app.use(express.urlencoded({ limit: '10mb', extended: true }));
 app.use(
     cors({
         origin: ["https://cash-pilot-frontend-qdgt.vercel.app", 
-            "https://cash-pilot-frontend-qdgt.vercel.app/"],
+            "https://cash-pilot-frontend-qdgt.vercel.app/" , "http://localhost:5173"],
         methods: ["GET", "POST", "PUT", "DELETE"],
         credentials: true 
     })
