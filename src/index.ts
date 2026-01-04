27475ea761b9314a206e4b4e719e3add5ea33826
dilini <dilinichamikasilva@gmail.com>
2026-01-04 23:03:31 +0530

    fix: update CORS origin to allow requests from both root and subpath of the production frontend

--- src/index.ts
+++ src/index.ts
@@ -22,7 +22,8 @@ app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
 app.use(
     cors({
-        origin: ["https://cash-pilot-frontend-qdgt.vercel.app/"],
+        origin: ["https://cash-pilot-frontend-qdgt.vercel.app", 
+            "https://cash-pilot-frontend-qdgt.vercel.app/"],
         methods: ["GET", "POST", "PUT", "DELETE"],
         credentials: true 
     })
