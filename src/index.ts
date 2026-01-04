d37a3683289c35f2f8539721632e91d79dba1d76
dilini <dilinichamikasilva@gmail.com>
2026-01-04 23:01:11 +0530

    fix: update CORS origin to allow requests from the production frontend

--- src/index.ts
+++ src/index.ts
@@ -22,7 +22,7 @@ app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
 app.use(
     cors({
-        origin: ["http://localhost:5173"],
+        origin: ["https://cash-pilot-frontend-qdgt.vercel.app/"],
         methods: ["GET", "POST", "PUT", "DELETE"],
         credentials: true 
     })
