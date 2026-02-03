3783a65564cb0a28468e32d81ec54b7069ad3a3d
dilini <dilinichamikasilva@gmail.com>
2026-02-04 00:17:16 +0530

    recovered the code

--- src/routes/account.routes.ts
+++ src/routes/account.routes.ts
@@ -1,12 +1,10 @@
 import {Router} from "express"
 import { getAccountById } from "../controller/auth.controller"
 import { authMiddleWare } from "../middleware/auth.middleware"
-import { isOwner } from "../middleware/role.middleware"
-import { inviteUserToBusiness } from "../controller/member.controller"
 
 const router = Router()
 
 router.get("/:id", authMiddleWare, getAccountById)
-router.post("/add-member", authMiddleWare, isOwner, inviteUserToBusiness);
+
 
 export default router
